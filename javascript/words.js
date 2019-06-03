a42fea3bc59a6d26b21a25213af6f35db97f61f1
AlexH-GC <alexander.hollington-sawyer@canada.ca>
2019-06-03 16:29:53 -0400

    -Updated to include Q110B -Updated to re-include single words -Restricted to 99 terms/phrases due to space -Updated to D3 v4 with queue

--- javascript/words.js
+++ javascript/words.js
@@ -1,26 +1,60 @@
 function init() {
 
-    d3.csv("data/words.csv", function (data2) {
-        d3.csv("data/words3.csv", function (data3) {
-            launch(data2, data3);
+   /* d3.csv("data/words_105.csv", function (data1_105) {
+        d3.csv("data/words_2_105.csv", function (data2_105) {
+            d3.csv("data/words_3_105.csv", function (data3_105) {
+                launch(data1_105, data2_105, data3_105);
+            });
         });
-    });
+    });*/
+
+
+     d3.queue()
+     .defer(d3.csv, "data/words_105.csv")
+     .defer(d3.csv, "data/words_2_105.csv")
+     .defer(d3.csv, "data/words_3_105.csv")
+     .defer(d3.csv, "data/words_110.csv")
+     .defer(d3.csv, "data/words_2_110.csv")
+     .defer(d3.csv, "data/words_3_110.csv")
+     .await(launch); //only function name is needed
+
 }
 
-function launch(data2, data3) {
+function launch(error, data1_105, data2_105, data3_105, data1_110, data2_110, data3_110) {
 
+    if (error) throw error;
+
+    var gram1BTN = document.getElementById('grams1');
     var gram2BTN = document.getElementById('grams2');
-    var gram3BTN = document.getElementById('grams3');
+    var q105BTN = document.getElementById('q105');
+    var q110bBTN = document.getElementById('q110b');
 
-    drawWordCloud(data2);
+    //Start with default checked options
+    drawWordCloud(data1_105);
 
     d3.select("#changeCount").on("click", function () {
-        if(gram2BTN.checked){
-            drawWordCloud(data2);
+
+        if(q105BTN.checked) {
+            if (gram1BTN.checked) {
+                drawWordCloud(data1_105);
+            }
+            else if (gram2BTN.checked) {
+                drawWordCloud(data2_105);
+            }
+            else //For 3 gram choice
+                drawWordCloud(data3_105);
         }
-        else if(gram3BTN.checked){
-            drawWordCloud(data3);
+        else{//For q110
+            if (gram1BTN.checked) {
+                drawWordCloud(data1_110);
+            }
+            else if (gram2BTN.checked) {
+                drawWordCloud(data2_110);
+            }
+            else //For 3 gram choice
+                drawWordCloud(data3_110);
         }
+
     });
 }
 
@@ -29,8 +63,8 @@ function drawWordCloud(data) {
 
     var nWords = d3.select("#nCount").property("value");
 
-    if (nWords >199){
-        nWords = 199;
+    if (nWords > 99) {
+        nWords = 99;
     }
 
     var list = [];
@@ -39,8 +73,8 @@ function drawWordCloud(data) {
         list[i] = data[i];
     }
     var svg_location = "#cloud";
-    var width = 1000;
-    var height = 1000;
+    var width = 900;
+    var height = 900;
 
     var max = d3.max(list, function (d) {
         return +d.count;
@@ -49,8 +83,32 @@ function drawWordCloud(data) {
         return +d.count;
     });
 
-   /* var colScale = d3.scale.quantile()
-        .domain([min, max])
+    /* var colScale = d3.scale.quantile()
+     .domain([min, max])
+     .range([
+     '#393b79',
+     '#5254a3',
+     '#6b6ecf',
+     '#9c9ede',
+     '#637939',
+     '#8ca252',
+     '#b5cf6b',
+     '#cedb9c',
+     '#8c6d31',
+     '#bd9e39',
+     '#e7ba52',
+     '#e7cb94',
+     '#843c39',
+     '#ad494a',
+     '#d6616b',
+     '#e7969c',
+     '#7b4173',
+     '#a55194',
+     '#ce6dbd',
+     '#de9ed6'
+     ]);*/
+
+    var colScale = d3.scaleOrdinal()
         .range([
             '#393b79',
             '#5254a3',
@@ -72,33 +130,9 @@ function drawWordCloud(data) {
             '#a55194',
             '#ce6dbd',
             '#de9ed6'
-        ]);*/
+        ]);
 
-   var colScale = d3.scale.ordinal()
-       .range([
-           '#393b79',
-           '#5254a3',
-           '#6b6ecf',
-           '#9c9ede',
-           '#637939',
-           '#8ca252',
-           '#b5cf6b',
-           '#cedb9c',
-           '#8c6d31',
-           '#bd9e39',
-           '#e7ba52',
-           '#e7cb94',
-           '#843c39',
-           '#ad494a',
-           '#d6616b',
-           '#e7969c',
-           '#7b4173',
-           '#a55194',
-           '#ce6dbd',
-           '#de9ed6'
-       ]);
-
-    var xScale = d3.scale.linear()
+    var xScale = d3.scaleLinear()
         .domain([min, max])
         .range([10, 75]);
 
@@ -116,8 +150,8 @@ function drawWordCloud(data) {
         })
         .rotate(0)
         /*.rotate(function () {
-            return ~~(Math.floor(Math.random() * 150)-75);
-        })*/
+         return ~~(Math.floor(Math.random() * 150)-75);
+         })*/
         .on("end", draw)
         .start();
 
@@ -135,7 +169,7 @@ function drawWordCloud(data) {
             })
             .style("font-family", "Helvetica")
             .style("font-weight", "700")
-            .style("fill", function (d,i) {
+            .style("fill", function (d, i) {
                 //return colScale(+d.count);
                 return colScale(i);
             })
